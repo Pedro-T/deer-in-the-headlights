8fae3fec44268918d01762a08170aba53c8bc8b3
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-13 11:58:32 -0500

    player script base + add to main scn

--- deer-in-the-headlights/player/player.gd
+++ deer-in-the-headlights/player/player.gd
@@ -1,4 +1,10 @@
 class_name Player extends CharacterBody2D
 
-const MOVE_SPEED: float = 32.0 # pixels per second
+const MOVE_SPEED: float = 64.0 # pixels per second
+
+func _ready() -> void:
+    pass
+
+func _physics_process(delta: float) -> void:
+    pass
 

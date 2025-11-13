2490cbbbaf5cddbd913e7fbfa0c4f19875b4d64c
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-13 16:33:05 -0500

    add first vehicle, vehicle movement

---
+++ deer-in-the-headlights/vehicle/vehicle.gd
@@ -0,0 +1,20 @@
+class_name Vehicle extends Area2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+var speed: float = 200.00 # pixels per second
+var move_direction: int = 1 # 1 for right, 0 for left
+
+func _ready() -> void:
+	if sprite:
+		collision_shape.shape.size = sprite.texture.get_size()
+	sprite.flip_h = move_direction == 1
+
+func _process(delta: float) -> void:
+	position.x += speed * delta * move_direction
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		GameManager.game_over(GameManager.GameEnding.RUN_OVER)

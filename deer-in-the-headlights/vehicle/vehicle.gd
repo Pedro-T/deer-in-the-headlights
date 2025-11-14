0b50d0ce45c92824f688c031b31763b0a163aac9
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-14 16:49:00 -0500

    move animation select to export bool for day/night modes

--- deer-in-the-headlights/vehicle/vehicle.gd
+++ deer-in-the-headlights/vehicle/vehicle.gd
@@ -5,11 +5,13 @@ class_name Vehicle extends Area2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
+@export var wipers_on: bool = true
 @export var speed: float = 200.00 # pixels per second
 @export var move_direction: int = 1 # 1 for right, 0 for left
 
+var used_animation: String = "drive_wipers" if wipers_on else "drive_day"
+
 func _ready() -> void:
-	var used_animation: String = "drive_wipers"
 	if sprite:
 		sprite.sprite_frames = sprite_frames
 		collision_shape.shape.size = sprite.sprite_frames.get_frame_texture(used_animation, 0).get_size()

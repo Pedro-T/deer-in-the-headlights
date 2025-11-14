1569296a46bcba90d621c5be7c97af165251c61a
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-14 16:46:01 -0500

    Change vehicle to use animated sprite, add three hatchback variants

--- deer-in-the-headlights/vehicle/vehicle.gd
+++ deer-in-the-headlights/vehicle/vehicle.gd
@@ -1,17 +1,19 @@
 class_name Vehicle extends Area2D
 
-@export var sprite_file: CompressedTexture2D
+@export var sprite_frames: SpriteFrames
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 @export var speed: float = 200.00 # pixels per second
 @export var move_direction: int = 1 # 1 for right, 0 for left
 
 func _ready() -> void:
+	var used_animation: String = "drive_wipers"
 	if sprite:
-		sprite.texture = sprite_file
-		collision_shape.shape.size = sprite.texture.get_size()
+		sprite.sprite_frames = sprite_frames
+		collision_shape.shape.size = sprite.sprite_frames.get_frame_texture(used_animation, 0).get_size()
+	sprite.play(used_animation)
 	sprite.flip_h = move_direction == 1
 
 func _process(delta: float) -> void:

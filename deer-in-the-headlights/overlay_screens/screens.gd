cde86b2e2aa590d51c5b1458b4be247e19b96370
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-16 07:32:27 -0500

    add placeholder title screen, game start button closes #7

--- deer-in-the-headlights/overlay_screens/screens.gd
+++ deer-in-the-headlights/overlay_screens/screens.gd
@@ -3,9 +3,35 @@ class_name Screens extends Node
 func show_game_over(_ending: GameManager.GameEnding) -> void:
     $GameOverScreen.visible = true
 
-func hide_game_over() -> void:
+func show_title() -> void:
+    $TitleScreen.visible = true
+
+func hide() -> void:
     $GameOverScreen.visible = false
+    $TitleScreen.visible = false
+
+
+
+
+func show_instructions() -> void:
+    pass
+
+func show_credits() -> void:
+    pass
+
+
+# button handlers
+func _on_credits_button_pressed() -> void:
+    show_credits()
+
+func _on_instructions_button_pressed() -> void:
+    show_instructions()
+
+func _on_start_button_pressed() -> void:
+    hide()
+    GameManager.start()
 
 func _on_title_return_button_pressed() -> void:
-    hide_game_over()
+    hide()
     GameManager.start()
+

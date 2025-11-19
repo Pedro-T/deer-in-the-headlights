ad8afe6d7275a54cda15ad37dea4896a6bc5cd30
Pedro Teixeira <pedroteixeira0@gmail.com>
2025-11-19 17:02:03 -0500

    broad rework of game over screen setup, add success outro screen & trigger

--- deer-in-the-headlights/overlay_screens/title_screen.gd
+++ deer-in-the-headlights/overlay_screens/title_screen.gd
@@ -2,12 +2,20 @@ class_name TitleScreen extends Node2D
 
 const SCROLL_SPEED: Vector2 = Vector2(-100.0, 0.0)
 
+@onready var start_button: Button = $StartButton
+
 func play_animation() -> void:
-	$Parallax2D.autoscroll = SCROLL_SPEED
-	$TitleDeer.play()
-	$TitleCar.play()
+    $Parallax2D.autoscroll = SCROLL_SPEED
+    $TitleDeer.play()
+    $TitleCar.play()
 
 func stop_animation() -> void:
-	$Parallax2D.autoscroll = Vector2.ZERO
-	$TitleDeer.stop()
-	$TitleCar.stop()
+    $Parallax2D.autoscroll = Vector2.ZERO
+    $TitleDeer.stop()
+    $TitleCar.stop()
+
+func _on_instructions_button_pressed() -> void:
+    $InstructionsPopup.popup()# Replace with function body.
+
+func _on_credits_button_pressed() -> void:
+    $CreditsPopup.popup()
